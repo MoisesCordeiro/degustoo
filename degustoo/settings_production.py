58a06f93a0cffb9789db681ca19f9d8cd8830d22
Irapoam Dias <irapoam.dias@gmail.com>
2016-03-13 22:00:06 -0300

    settings production

--- degustoo/settings_production.py
+++ degustoo/settings_production.py
@@ -1,4 +1,17 @@
 from .settings import *
 
 DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'isp_dados',
+    #     'HOST': 'localhost',
+    #     'USER': 'isp_dados',
+    #     'PASSWORD': 'isp_dados',
+    #     'PORT': '5432',
+    # }
+}
