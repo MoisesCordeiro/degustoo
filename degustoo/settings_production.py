de99329f07ba1ff99a008d18366d30725d07b76e
Irapoam Dias <irapoam.dias@gmail.com>
2016-03-13 22:26:11 -0300

    unicode

--- degustoo/settings_production.py
+++ degustoo/settings_production.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ḍegustoo',
+        'NAME': 'degustoo',
         'HOST': 'localhost',
         'USER': 'userdegustoo',
         'PASSWORD': 'passdegustoo',
