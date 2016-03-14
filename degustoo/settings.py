02a250b437a04b4c6e98b007e8c12b487d905346
Irapoam Dias <irapoam.dias@gmail.com>
2016-03-13 22:02:57 -0300

    static collected

--- degustoo/settings.py
+++ degustoo/settings.py
@@ -91,6 +91,7 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
 # Custom configuration
 AUTH_USER_MODEL = 'core.Usuario'
